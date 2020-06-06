2549fbe3d163ab86acbbc88c3b8ad437df25718e
Defetysta <tadeusznowak95@gmail.com>
2020-06-06 14:32:43 +0200

    Revamped shooting

--- Assets/Scripts/CONST_VALUES.cs
+++ Assets/Scripts/CONST_VALUES.cs
@@ -2,5 +2,7 @@
 {
     internal const string GAME_OVER_SCENE_NAME = "Game Over";
     internal const float MAX_VELOCITY_FORWARD = 10f;
-    internal const float MAX_VELOCITY_ROTATION = 300f;
+    internal const float MAX_VELOCITY_ROTATION = 240f;
+    internal const float PROJECTILE_SPEED = 20;
+    internal const float FIRING_INTERVAL = 0.1f;
 }
--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
@@ -7,37 +8,43 @@ public class PlayerFire : MonoBehaviour
     private GameObject bulletPrefab;
 #pragma warning restore 0649
     private Camera cam;
-    public float fireDelay = 0.25f;
     //float cooldownTimer = 0f;
     private Vector2 cursorInWorldPos;
     private Vector2 direction;
     private PoolingManager poolingManager;
-    private void Start()
-    {
-        poolingManager = FindObjectOfType<PoolingManager>();
-    }
+    private string projectilesPoolTag = "PlayerProjectile";
+    private GameObject newProjectile;
 
+    private WaitUntil awaitInput = new WaitUntil(() => Input.GetMouseButton(0));
+    private WaitForSeconds awaitEndOfInterval = new WaitForSeconds(CONST_VALUES.FIRING_INTERVAL);
     private void Awake()
     {
         cam = FindObjectOfType<CameraShake>().GetComponent<Camera>();
     }
-
-    private void Update()
+    private void OnEnable()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            FireProjectile();
-        }
+        StartCoroutine(AwaitPlayerInput());
     }
 
+    private void Start()
+    {
+        poolingManager = FindObjectOfType<PoolingManager>();
+    }
+    private IEnumerator AwaitPlayerInput()
+    {
+        yield return awaitInput;
+        FireProjectile();
+        yield return awaitEndOfInterval;
+        yield return AwaitPlayerInput();
+    }
     private void FireProjectile()
     {
         cursorInWorldPos = cam.ScreenToWorldPoint((Vector2)Input.mousePosition);
         direction = cursorInWorldPos - (Vector2)transform.position;
         direction.Normalize();
-        GameObject projectile = poolingManager.SpawnFromPool("PlayerProjectile", transform.position, Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg));
-        projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        newProjectile = poolingManager.SpawnFromPool(projectilesPoolTag, transform.position, Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg));
+        newProjectile.GetComponent<Rigidbody2D>().velocity = direction * CONST_VALUES.PROJECTILE_SPEED;
     }
 
-    public float speed;
+
 }
